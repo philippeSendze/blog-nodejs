457d5e7e44eb42cbfd4e9c785d72427ba28a4a13
Philippe Sendze <sendze.philippe@gmail.com>
2022-08-09 04:12:46 +0100

    add a key

--- routes/article.route.js
+++ routes/article.route.js
@@ -1,17 +1,19 @@
+require("dotenv").config();
+
 module.exports = (app) => {
   const article = require("../controllers/article.controller.js");
   var router = require("express").Router();
   // Create a new article
-  router.post("/", article.create); // A POST request on POSTMAN must be created with the option x-www-form-urlencoded in the 'Body' part.
+  router.post(`/${process.env.KEY}`, article.create); // A POST request on POSTMAN must be created with the option x-www-form-urlencoded in the 'Body' part.
   // Retrieve all articles
   router.get("/", article.findAll);
   // Retrieve a single article with id
   router.get("/:id", article.findOne);
   // Update a article with id
-  router.put("/:id", article.update);
+  router.put(`/${process.env.KEY}/:id`, article.update);
   // Delete a article with id
-  router.delete("/:id", article.delete);
+  router.delete(`/${process.env.KEY}/:id`, article.delete);
   // Delete all articles
-  router.delete("/", article.deleteAll);
+  router.delete(`/${process.env.KEY}`, article.deleteAll);
   app.use("/api/articles", router);
 };
